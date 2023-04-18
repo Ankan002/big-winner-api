66c450ee93005ecd09210ac1168406835a502af1
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-19 00:27:13 +0530

    feat: :sparkles: add is email verified

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,9 @@
-import { getUser } from "controllers/user";
+import { getUser, isEmailVerified } from "controllers/user";
 import { Router } from "express";
 import { isAuthenticated } from "middlewares/auth";
 
 export const userRouter = Router();
 
 userRouter.route("/").get(isAuthenticated, getUser);
+
+userRouter.route("/is-email-verified").get(isAuthenticated, isEmailVerified);
