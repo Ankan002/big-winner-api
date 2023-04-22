3743daa4ef7309d23bfb7acb52a6f43293ffba12
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-22 13:55:30 +0530

    feat: :sparkles: add daily win jackpot

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import { profileRouter } from "routes/profile";
 import { verifyEmailRouter } from "routes/verify-email";
 import { walletRouter } from "routes/wallet";
 import { dailyLoginRouter } from "routes/daily-login";
+import { dailWinJackpotRouter } from "routes/game";
 
 export const startServer = () => {
 	const app = express();
@@ -37,6 +38,7 @@ export const startServer = () => {
 	app.use("/api/verify-email", verifyEmailRouter);
 	app.use("/api/wallet", walletRouter);
 	app.use("/api/daily-login", dailyLoginRouter);
+	app.use("/api/game", dailWinJackpotRouter);
 
 	app.listen(PORT, () => logger.info(`App is running at PORT: ${PORT}`));
 };
